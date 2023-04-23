efc505ce8c5d48f1d39c4fb4d33bb0d9e16deb4b
John Tannous <john.tannous97@gmail.com>
2023-04-22 23:22:16 -0700

    added pictures and create basic navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import logo from '../../assets/logo.png'
 
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+        <img src={logo} alt="app__logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li ><a href="#home">Home</a></li>
+        <li ><a href="#about">About</a></li>
+        <li ><a href="#favorites">Favorites</a></li>
+        <li ><a href="#menu">Menu</a></li>
+        <li ><a href="#gallery">Gallery</a></li>
+        <li ><a href="#contact">Contact</a></li>
+      </ul>
+  </nav>
 );
 
 export default Navbar;
